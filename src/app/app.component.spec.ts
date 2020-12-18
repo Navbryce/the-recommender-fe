c63d2f9f679392ce7a7e7693f2a4c7a7a24262eb
Bryce Plunkett <plunkett.bryce.m@gmail.com>
2020-12-18 16:41:29 -0500

    Get tests to pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -8,6 +8,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
+import { SearchClientModule } from './search-client/search-client.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -22,6 +23,7 @@ describe('AppComponent', () => {
         MatAutocompleteModule,
         MatInputModule,
         MatSelectModule,
+        SearchClientModule,
       ],
     }).compileComponents();
   }));
